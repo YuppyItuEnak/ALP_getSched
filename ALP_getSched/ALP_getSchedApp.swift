45fea609c5ed4a5d576213344fb8970f167dd375
YuppyItuEnak <94430554+YuppyItuEnak@users.noreply.github.com>
2023-05-09 10:05:10 +0700

    Initial Commit

---
+++ ALP_getSched/ALP_getSchedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_getSchedApp.swift
+//  ALP_getSched
+//
+//  Created by MacBook Pro on 09/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ALP_getSchedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
