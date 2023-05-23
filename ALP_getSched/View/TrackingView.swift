30d63c0221fbe76ba190f67cae06693d6526dac9
DaveN8 <100126795+DaveN8@users.noreply.github.com>
2023-05-23 08:21:46 +0700

    Membuat file

---
+++ ALP_getSched/View/TrackingView.swift
@@ -0,0 +1,20 @@
+//
+//  TrackingView.swift
+//  ALP_getSched
+//
+//  Created by MacBook Pro on 23/05/23.
+//
+
+import SwiftUI
+
+struct TrackingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TrackingView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrackingView()
+    }
+}
