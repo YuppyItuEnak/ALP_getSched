cce21700bc2b41e44612f10696ed9f8a7e697c5b
DaveN8 <100126795+DaveN8@users.noreply.github.com>
2023-05-29 09:13:15 +0700

    Command profile viewmodel

--- ALP_getSched/ViewModel/ProfileViewModel.swift
+++ ALP_getSched/ViewModel/ProfileViewModel.swift
@@ -12,11 +12,11 @@ class ProfielViewModel: ObservableObject {
     
     func UserRegister(userName: String, userPassword: String, userEmail: String, selectedCondition: Bool, userId: Int) {
         let newProfile = ProfileModel(userName: userName, userPassword: userPassword, userEmail: userEmail)
-        if selectedCondition = true {
-            profile.append(newProfile)
-        } else {
-            
-        }
+//        if selectedCondition = true {
+//            profile.append(newProfile)
+//        } else {
+//            
+//        }
     }
     
     func UserLogin(userName: String, userPassword: String){
